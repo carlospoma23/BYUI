c16d4f74364e76ee204dcf5485f342c031faa205
carlospoma23 <carlospoma23@gmail.com>
2022-09-18 17:21:41 -0600

    first commit

---
+++ CES110/favoritecolor.py
@@ -0,0 +1,15 @@
+name = input('Hello,  What is your name ? ')
+print('hello ' + name + ' I would like to ask some questions ')
+color=input('Please type your favorite color : ')
+hobby= input('What is your favorite hobby? ')
+dessert= input('If you had to choose a dessert, which one would you like ? ')
+print()
+print('Thank you for your time, your answers are :')
+print()
+print('Your favorite color is ' + color)
+print('Your favorite hobby is ' + hobby)
+print('Your dessert is ' + dessert)
+
+
+
+
---
+++ CES110/hellowordl.py
@@ -0,0 +1,6 @@
+
+
+name = input('What is your name?')
+print('hello ' + name)
+print('Nice to meet you '+ name + ' ,Now we are friends.')
+
