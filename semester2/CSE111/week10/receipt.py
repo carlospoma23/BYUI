da1db24cb859e7f65ed9cadc05943fd016c56dde
carlospoma23 <carlospoma23@gmail.com>
2023-03-11 08:19:46 -0700

    week 10

--- semester2/CSE111/week10/receipt.py
+++ semester2/CSE111/week10/receipt.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import string, random
 #are the indexes of the elements in products dictionary, each value is a list
 PRODUCT_INDEX=0
 NAME_INDEX=1
@@ -10,46 +11,94 @@ PRICE_INDEX=2
 QUANTITY_INDEX=1
 
 def main():
-    id_product=0
 
-    products_dict=read_dictionary('products.csv',id_product)
+    try:
 
-    #reading the CVS request and storing and value in list_request
+        id_product=0
+        products_dict=read_dictionary('products.csv',id_product)
+        #reading the CVS request and storing and value in list_request
+        list_request=[]
+        print()
+        store_name="Inkom Emporium"
+        print(f"**** {store_name} **** \n")
+        number_items=0
+        subtotal=0
 
-    list_request=[]
+        with open('request.csv','rt') as csv_file:
+            reader=csv.reader(csv_file)
+            next(reader)
+            for code, quantity in reader:
+                list_request.append(code + "," + quantity)
+                code_product=code
+                request_quantity=int(quantity)
 
-    print()
-    store_name="Inkom Emporium"
-    print(f"**** {store_name} **** \n")
-    number_items=0
-    subtotal=0
-    with open('request.csv','rt') as csv_file:
-        reader=csv.reader(csv_file)
-        next(reader)
+            #    if code_product in products_dict:
+                products_list=products_dict[code_product]
+                name_product=products_list[NAME_INDEX]
+                price_product=float(products_list[PRICE_INDEX])
+                number_items += request_quantity
+                subtotal += price_product*request_quantity
 
-        for code, quantity in reader:
+                print(f"{name_product}: {request_quantity}  @ {price_product}")
+        
+        #genering Coupon random
+
+        number_of_coupon=1
+        length_of_coupon=8
+        
+        for x in range(number_of_coupon):
+            coupon=''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_coupon))
+        
+      
+       
+
+                
+        current_date_time=datetime.now()
+        # get a integer number related to the week day (Where Monday is 0 and Sunday is 6)
+        current_date=current_date_time.weekday()
+        #testing current_date
+        #current_date=6
+        
+        current_time=current_date_time.time()
+        currect_hour=current_time.hour
+        #testomg current_hour
+        #currect_hour=13
+    
+        discount=0
+        #computing discount if is tuesday or wednesday or the time is before 11 am
+        if (current_date==1 or current_date==2 or currect_hour<11):
+            rate_discount_day=0.10
+            discount=rate_discount_day*subtotal
+            sales_tax=0.06*(subtotal-discount)
+            total=subtotal-discount+sales_tax
+
+        else:
+            sales_tax=0.06*(subtotal-discount)
+            total=subtotal-discount+sales_tax
             
-            list_request.append(code + "," + quantity)
-            code_product=code
-            request_quantity=int(quantity)
+       
+        
+        print(f"\nNumber of Items: {number_items}")
+        print(f"Subtotal: {subtotal:.2f}")
+        print(f'Discount: {discount:.2f}')
+        print(f"Sales Tax: {sales_tax:.2f}")
+        print(f"Total: {total:.2f}")
+        print(f'\nCOUPON: {coupon}')
+        print("***********************")
+        print("Please don't forget to use this 'Coupon' the next time and you will receive 5 % off for your purchases.")
+    
+        print(f"\nThank you for shopping at the {store_name}")
+        
 
-            products_list=products_dict[code_product]
-            name_product=products_list[NAME_INDEX]
-            price_product=float(products_list[PRICE_INDEX])
-            number_items += request_quantity
-            subtotal += price_product*request_quantity
-
-            print(f"{name_product}: {request_quantity}  @ {price_product}")
+        print(f'{current_date_time:%a %b %d   %I:%M:%S   %Y} ')
+       
+    except (FileNotFoundError, PermissionError) as error:
+        print(error)
+        print(f"Please make sure that the file exist.")
     
-    sales_tax=0.06*subtotal
-    total=subtotal+sales_tax
-    
-    print(f"\nNumber of Items: {number_items}")
-    print(f"Subtotal: {subtotal:.2f}")
-    print(f"Sales Tax: {sales_tax:.2f}")
-    print(f"Total: {total:.2f}")
-    
-    print(f"\n Thank you for shopping at the {store_name}")
+    except(csv.Error, KeyError) as error:
+        print(error)
+        print(f'Error: unknown product ID {error}  of {csv_file.name} line {reader.line_num}')
 
 
 def read_dictionary(filename, key_column_index):
